034a6793c5ae5201f6b17f97639a46aa1e55a218
SindreMA <sindrema@gmail.com>
2023-09-16 20:05:45 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 95);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(175, 23);
+            this.metroTextBox1.TabIndex = 0;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.TextChanged += new System.EventHandler(this.metroTextBox1_TextChanged);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(153, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(23, 164);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(175, 23);
+            this.metroTextBox2.TabIndex = 1;
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox2.TextChanged += new System.EventHandler(this.metroTextBox1_TextChanged);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(77, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Input folder";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 131);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(89, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "Output folder";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(302, 79);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(131, 39);
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "Start";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // metroTextBox3
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox3.CustomButton.Image = null;
+            this.metroTextBox3.CustomButton.Location = new System.Drawing.Point(19, 1);
+            this.metroTextBox3.CustomButton.Name = "";
+            this.metroTextBox3.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox3.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox3.CustomButton.TabIndex = 1;
+            this.metroTextBox3.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox3.CustomButton.UseSelectable = true;
+            this.metroTextBox3.CustomButton.Visible = false;
+            this.metroTextBox3.Lines = new string[0];
+            this.metroTextBox3.Location = new System.Drawing.Point(23, 233);
+            this.metroTextBox3.MaxLength = 32767;
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.PasswordChar = '\0';
+            this.metroTextBox3.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox3.SelectedText = "";
+            this.metroTextBox3.SelectionLength = 0;
+            this.metroTextBox3.SelectionStart = 0;
+            this.metroTextBox3.ShortcutsEnabled = true;
+            this.metroTextBox3.Size = new System.Drawing.Size(41, 23);
+            this.metroTextBox3.TabIndex = 5;
+            this.metroTextBox3.UseSelectable = true;
+            this.metroTextBox3.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox3.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox3.TextChanged += new System.EventHandler(this.metroTextBox1_TextChanged);
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(23, 202);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(79, 19);
+            this.metroLabel3.TabIndex = 6;
+            this.metroLabel3.Text = "Update rate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 300);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroTextBox3);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Name = "Form1";
+            this.Text = "PDF Auto Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Converter
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+           
+            InitializeComponent();
+            metroTextBox1.Text = ConfigurationManager.AppSettings["InputFolder"];
+            metroTextBox2.Text = ConfigurationManager.AppSettings["OutputFolder"];
+            metroTextBox3.Text = ConfigurationManager.AppSettings["RefreshRate"];
+
+        }
+        private string UD;
+        private string AE;
+        private string NK;
+        private string AER;
+        private string SK;
+        public int i;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            UD = null;
+            AE = null;
+            NK = null;
+            AER = null;
+            SK = null;
+
+
+            i = 0;
+            try
+            {
+                
+                foreach (var item in Directory.GetFiles(metroTextBox1.Text))
+                {
+                    i++;
+                }
+                
+                if (i != 0)
+                {
+                    foreach (var item in Directory.GetFiles(metroTextBox1.Text))
+                    {
+                        string localtxt = Environment.CurrentDirectory + "\\" + item.Replace(".pdf", "").Replace(metroTextBox1.Text, "") + ".txt";
+                        string strCmdText = "/C " + @"d:\pdftotext.exe """  + item + "\" \"" + localtxt + "\"";
+                        //****************************************************************************************************************************
+                        ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", strCmdText) { RedirectStandardError = true, RedirectStandardOutput = true, UseShellExecute = false, CreateNoWindow = true };
+
+                        using (Process proc = new Process())
+                        {
+                            proc.StartInfo = procStartInfo;
+                            proc.Start();
+                            string output = proc.StandardOutput.ReadToEnd();
+                            if (string.IsNullOrEmpty(output)) output = proc.StandardError.ReadToEnd();
+                            this.Hide();
+                        }
+
+                        Thread.Sleep(100);
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        bool writing = false;
+                        List<string> doc = new List<string>();
+
+                        XmlDocument xmlDoc = new XmlDocument();
+                        //using (StreamReader sr = new StreamReader(metroTextBox2.Text + "\\" + item.Replace(".pdf", "").Replace(metroTextBox1.Text, "") + ".txt"))
+                        //{
+                        //    while (sr.Peek() >= 0)
+                        //    {
+                        //        doc.Add(sr.ReadLine());
+                        //    }
+                        //}
+                        foreach (var line in File.ReadAllLines(localtxt,Encoding.UTF7))
+                        {
+                            if (line.Contains("Utdanning"))
+                            {
+                                writing = true;
+                            }
+                            if (line.Contains("Arbeidserfaring"))
+                            {
+                                writing = false;
+                            }
+                            if (writing && !line.Contains("Utdanning"))
+                            {
+                                UD = UD + line + " ";
+                            }
+                        }
+                        foreach (var line in File.ReadAllLines(localtxt, Encoding.UTF7))
+                        {
+                            if (line.Contains("Arbeidserfaring"))
+                            {
+                                writing = true;
+                            }
+                            if (line.Contains("Nøkkelkompetanse"))
+                            {
+                                writing = false;
+                            }
+                            if (writing && !line.Contains("Arbeidserfaring"))
+                            {
+                                AE = AE + line + " ";
+                            }
+                        }
+
+
+
+                        foreach (var line in File.ReadAllLines(localtxt, Encoding.UTF7))
+                        {
+                            if (line.Contains("Nøkkelkompetanse"))
+                            {
+                                writing = true;
+                            }
+                            if (line.Contains("Annen erfaring"))
+                            {
+                                writing = false;
+                            }
+                            if (writing && !line.Contains("Nøkkelkompetanse"))
+                            {
+                                NK = NK +  line;
+                            }
+                        }
+                        foreach (var line in File.ReadAllLines(localtxt, Encoding.UTF7))
+                        {
+                            if (line.Contains("Annen erfaring"))
+                            {
+                                writing = true;
+                            }
+                            if (line.Contains("Språk"))
+                            {
+                                writing = false;
+                            }
+                            if (writing && !line.Contains("Annen erfaring"))
+                            {
+                                AER = AER + line;
+                            }
+                        }
+                        foreach (var line in File.ReadAllLines(localtxt, Encoding.UTF7))
+                        {
+                            if (line.Contains("Språk"))
+                            {
+                                writing = true;
+                            }
+                            if (line.Contains("Ønsker for neste jobb"))
+                            {
+                                writing = false;
+                            }
+                            if (writing && !line.Contains("Språk"))
+                            {
+                                SK = SK + line;
+                            }
+
+                        }
+
+                        XmlWriterSettings settings = new XmlWriterSettings();
+                        settings.Indent = true;
+                        settings.IndentChars = ("    ");
+                        settings.CloseOutput = true;
+                        //settings.OmitXmlDeclaration = true;
+                        using (XmlWriter writer = XmlWriter.Create(metroTextBox2.Text + "\\" + item.Replace(".pdf", "").Replace(metroTextBox1.Text, "") + ".xml", settings))
+                        {
+                            writer.WriteStartElement("data");
+                            writer.WriteElementString("Utdanning", UD);
+                            writer.WriteElementString("Arbeidserfaring", AE);
+                            writer.WriteElementString("Nøkkelkompetanse", NK);
+                            writer.WriteElementString("AnnenErfaring", AER);
+                            writer.WriteElementString("Språk", SK.Replace("\r\n\fSpråk\r\n",""));
+                            writer.WriteEndElement();
+                            writer.Flush();
+                        }
+
+                       
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        ////////////////////////////////////////////////////////////////////////////////////
+                        File.Delete(item);
+                    }
+                }
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+                timer1.Enabled = false;
+                metroButton1.Text = "Start";
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                metroButton1.Text = "Start";
+            }
+            if (!timer1.Enabled)
+            {
+                timer1.Enabled = true;
+                metroButton1.Text = "Stop";
+            }
+        }
+
+        private void metroTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Interval = int.Parse(metroTextBox3.Text);
+            }
+            catch (Exception)
+            {
+            }
+            
+        }
+    }
+}
